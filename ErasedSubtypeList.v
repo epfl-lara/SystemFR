e0bf0ebd21ea8174e6b62f72f849a7b06ad41a5d
Jad Hamza <jad.hamza@epfl.ch>
2020-03-20 13:52:14 +0100

    Fix proofs

--- ErasedSubtypeList.v
+++ ErasedSubtypeList.v
@@ -2,65 +2,6 @@ Require Export SystemFR.ErasedList.
 
 Opaque reducible_values.
 
-Ltac simp_red2 :=
-  rewrite reducible_values_equation_1 in * |- ||
-  rewrite reducible_values_equation_2 in * |- ||
-  rewrite reducible_values_equation_3 in * |- ||
-  rewrite reducible_values_equation_4 in * |- ||
-  rewrite reducible_values_equation_5 in * |- ||
-  rewrite reducible_values_equation_6 in * |- ||
-  rewrite reducible_values_equation_7 in * |- ||
-  rewrite reducible_values_equation_8 in * |- ||
-  rewrite reducible_values_equation_9 in * |- ||
-  rewrite reducible_values_equation_10 in * |- ||
-  rewrite reducible_values_equation_11 in * |- ||
-  rewrite reducible_values_equation_12 in * |- ||
-  rewrite reducible_values_equation_13 in * |- ||
-  rewrite reducible_values_equation_14 in * |- ||
-  rewrite reducible_values_equation_15 in * |- ||
-  rewrite reducible_values_equation_16 in * |- ||
-  rewrite reducible_values_equation_17 in * |- ||
-  rewrite reducible_values_equation_18 in * |- ||
-  rewrite reducible_values_equation_19 in * |- ||
-  rewrite reducible_values_equation_20 in * |- ||
-  rewrite reducible_values_equation_21 in * |- ||
-  rewrite reducible_values_equation_22 in * |- ||
-  rewrite reducible_values_equation_23 in * |- ||
-  rewrite reducible_values_equation_24 in * |- ||
-  rewrite reducible_values_equation_25 in * |- ||
-  rewrite reducible_values_equation_26 in * |- ||
-  rewrite reducible_values_equation_27 in * |- ||
-  rewrite reducible_values_equation_28 in * |- ||
-  rewrite reducible_values_equation_29 in * |- ||
-  rewrite reducible_values_equation_30 in * |- ||
-  rewrite reducible_values_equation_31 in * |- ||
-  rewrite reducible_values_equation_32 in * |- ||
-  rewrite reducible_values_equation_33 in * |- ||
-  rewrite reducible_values_equation_34 in * |- ||
-  rewrite reducible_values_equation_35 in * |- ||
-  rewrite reducible_values_equation_36 in * |- ||
-  rewrite reducible_values_equation_37 in * |- ||
-  rewrite reducible_values_equation_38 in * |- ||
-  rewrite reducible_values_equation_39 in * |- ||
-  rewrite reducible_values_equation_40 in * |- ||
-  rewrite reducible_values_equation_41 in * |- ||
-  rewrite reducible_values_equation_42 in * |- ||
-  rewrite reducible_values_equation_43 in * |- ||
-  rewrite reducible_values_equation_44 in * |- ||
-  rewrite reducible_values_equation_45 in * |- ||
-  rewrite reducible_values_equation_46 in * |- ||
-  rewrite reducible_values_equation_47 in * |- ||
-  rewrite reducible_values_equation_48 in * |- ||
-  rewrite reducible_values_equation_49 in * |- ||
-  rewrite reducible_values_equation_50 in * |- ||
-  rewrite reducible_values_equation_51 in * |- ||
-  rewrite reducible_values_equation_52 in * |- ||
-  rewrite reducible_values_equation_53 in * |- ||
-  rewrite reducible_values_equation_54 in * |- ||
-  rewrite reducible_values_equation_55 in * |- ||
-  rewrite reducible_values_equation_56 in * |- ||
-  rewrite reducible_values_equation_57 in *.
-
 Lemma subst_list:
   forall l tag,
     psubstitute List l tag = List.
@@ -84,8 +25,8 @@ Lemma nil_subtype_list:
   forall tvars gamma,
     [ tvars; gamma ⊨ Nil <: List ].
 Proof.
-  unfold subtype;
-    repeat step || rewrite subst_list || simp_red2.
+  unfold open_subtype, subtype;
+    repeat step || rewrite subst_list || simp_red_top_level_hyp.
 
   eapply reducibility_equivalent; eauto using equivalent_sym;
     repeat step;
@@ -105,11 +46,8 @@ Lemma cons_subtype_list:
     [ tvars; gamma ⊨ T <: List ] ->
     [ tvars; gamma ⊨ Cons H T <: List ].
 Proof.
-  unfold subtype;
-    repeat step || rewrite subst_list || simp_red2.
-
-  rewrite (open_none _ 1) in H12 by eauto with wf.
-  repeat (rewrite open_none in * by t_closer).
+  unfold open_subtype, subtype;
+    repeat step || rewrite subst_list || simp_red_top_level_hyp || open_none.
 
   eapply reducibility_equivalent; eauto using equivalent_sym;
     repeat step;
@@ -119,16 +57,15 @@ Proof.
   apply reducible_values_fold_gen with 0;
     repeat step || simp_spos.
 
-  simp_red2;
-    repeat step.
+  - simp_red_top_level_hyp; repeat step.
 
-  - apply reducible_expr_value; steps; eauto with values.
-    apply reducible_left; steps;
-      eauto using reducible_value_expr, reducible_top.
+    + apply reducible_expr_value; steps; eauto with values.
+      apply reducible_left; steps;
+        eauto using reducible_value_expr, reducible_top.
 
-  - apply reducible_expr_value; steps; eauto with values.
-    apply reducible_right; steps;
-      eauto using reducible_value_expr, reducible_top.
+    + apply reducible_expr_value; steps; eauto with values.
+      apply reducible_right; steps;
+        eauto using reducible_value_expr, reducible_top.
 
   - apply reducible_expr_value; steps; eauto with values.
     apply reducible_right; steps.
