7d1919049ae918951f48c2201f784c2ba60913dd
Blaudeau Clément <clement.blaudeau@gmail.com>
2020-03-26 11:24:34 +0100

    Exponential recursion...

--- Notations.v
+++ Notations.v
@@ -1,5 +1,6 @@
 Require Export SystemFR.Trees.
 Require Export SystemFR.Syntax.
+Require Export SystemFR.Freshness.
 
 
 Open Scope list.
@@ -14,8 +15,8 @@ Notation "|[ x ]|" := x (in custom expr, x constr).
 Notation "x" := x (in custom expr at level 0, x ident).
 
 (* Variables (nameless) *)
-Notation "'ft{' v '}'" := (fvar v term_var) (in custom expr at level 4,
-                                                v constr at level 4).
+Notation "'ft{' v '}'" := (fvar v term_var) (in custom expr,
+                                                v constr).
 Notation "'fT{' t '}'" := (fvar t type_var) (in custom type,
                                                 t constr).
 Notation "'lt{' v '}'" := (lvar v term_var) (in custom expr,
@@ -68,9 +69,9 @@ Notation " t 'match'  '|' t1 '|' t2 'end'" := (tmatch t t1 t2) (in custom expr a
                                                               t2 custom expr).
 
 (* Fix points *)
-Notation "'fix' t" := (notype_tfix t) (in custom expr at level 2,
+(* Notation "'fix' t" := (notype_tfix t) (in custom expr at level 2,
                                           t custom expr).
-
+*)
 (* Let expression *)
 Notation "'let' := t1 'in' t2" := (notype_tlet t1 t2) (in custom expr at level 1,
                                                        t1 custom expr at level 4,
@@ -89,17 +90,48 @@ Notation "'fun' [ T ] => y" := (lambda T y) (in custom expr at level 4,
                                                T custom type at level 4,
                                                y custom expr at level 4).
 
+Definition fresh_id (t: tree) := makeFresh (fv t :: nil).
+
 Notation "'fun' ( x ) => t" :=
-  (let x := (fvar 1000 term_var) in
-   (notype_lambda (close 0 t 1000))) (in custom expr at level 100, x ident, t custom expr).
+   (let fresh := (S (let x := (fvar 0 term_var) in (max (fv t)))) in 
+    let x     := (fvar fresh term_var) in
+    notype_lambda (close 0 t fresh))
+     (in custom expr at level 100, x ident, t custom expr).
+
+Eval compute in [| fun ( x ) => x |].
+
+  (*
+Notation "'fun' ( x ) => t" :=
+   (let fresh := fresh_id (let x := (fvar 0 term_var) in t) in
+   ((fun x => (fvar fresh term_var))
+   (notype_lambda (close 0 t fresh)))) (in custom expr at level 100, x ident, t custom expr). *)
+
+Eval compute in [| fun ( x ) => (fun (y) => x) |].
+Example test1 : [| fun ( x ) => (x, (fun (y) => (x, y)), x) |] = [| fun => ((lt{0}, (fun => (lt{1}, lt{0}))), lt{0}) |].
+Proof.
+  unfold fv, pfv, max.
+  simpl.
+  assert (forall t:nat, (if tag_eq_dec term_var term_var then singleton t else nil) = (singleton t)) as H. {
+    destruct (tag_eq_dec term_var term_var) eqn:E.
+    reflexivity.
+    congruence. }
+  rewrite ?H.
+  simpl.
+  reflexivity.
+Qed.
+
+Eval compute in [| fun ( x ) => (fun (y) => x)|].
 
 Notation "'def_rec' f '(' x ')' := t" :=
-  (let f := (lvar 1000 term_var) in
+  (let f := (fvar 1000 term_var) in
    let x := (fvar 999 term_var) in
-   (notype_tfix (close 0 (close 1 t 999) 1000))
+   (notype_tfix (close 0 (close 1 t 999) 1000)))
   (in custom expr at level 100, f ident, x ident, t custom expr, right associativity).
 
 
+
+Eval compute in [|
+     fun (x) => (fun (y) => ((x,y), fun(z) => (x, y))) |].
 
 Eval compute in [|
 (
