4e8577e3b0c0c747211bc5c596408e8824b7d657
Blaudeau Clément <clement.blaudeau@gmail.com>
2020-04-23 14:36:27 +0200

    Ugly version of evaluator proof

--- Evaluator.v
+++ Evaluator.v
@@ -4,6 +4,9 @@ Require Export SystemFR.Freshness.
 Require Export SystemFR.Notations.
 Import Notations.UnTyped.
 Require Export SystemFR.PrimitiveSize.
+Require Export SystemFR.SmallStep.
+
+Open Scope bool_scope.
 
 Fixpoint isNat (t : tree) : bool :=
   match t with
@@ -13,8 +16,8 @@ Fixpoint isNat (t : tree) : bool :=
 end.
 Fixpoint isValue (t: tree) : bool :=
   match t with
-    | lvar _ _ => true
-    | fvar _ _ => true
+    (*| lvar _ _ => true
+    | fvar _ _ => true *)
     | uu => true
     | ttrue => true
     | tfalse => true
@@ -22,14 +25,53 @@ Fixpoint isValue (t: tree) : bool :=
     | tleft e1 => (isValue e1)
     | tright e1 => (isValue e1)
     | zero => true
-    | succ e1 => (isNat e1)
-    | lambda _ _ => true
+    | succ e1 => (isValue e1)
     | notype_lambda _ => true
-    | err _ => true
-    | notype_err => true
-    | tfix _ _  => true
     | _ => false end.
- 
+
+Hint Rewrite Bool.andb_true_iff: bools.
+Hint Rewrite Bool.andb_false_iff: bools.
+Hint Rewrite Bool.orb_true_iff: bools.
+Hint Rewrite Bool.orb_false_iff: bools.
+Hint Rewrite Bool.negb_true_iff: bools.
+Hint Rewrite Bool.negb_false_iff: bools.
+
+Ltac bools :=
+  autorewrite with bools in *.
+
+Lemma isValueCorrect : forall v, isValue v = true <-> cbv_value v.
+Proof.
+  split.
+  - induction v; repeat step || bools; eauto with values.
+  - induction 1; repeat step || bools.
+Qed.
+
+Definition getPair t : {t': option (tree*tree) | forall t1 t2, t' = Some (t1, t2) <-> t = pp t1 t2}.
+  Proof.
+    exists ( match t with
+    | pp e1 e2 => Some (e1,e2)
+    | _ => None end).
+    destruct t; steps.
+  Qed.
+
+  Definition getApp t : {t': option tree | forall t1, t' = Some t1 <-> t = notype_lambda t1}.
+  Proof.
+    exists (match t with
+       | notype_lambda body => Some body
+       | _ => None end).
+    destruct t; steps.
+Qed.  
+
+  Definition getLR t : {t': option tree | forall t1, t' = Some t1 <-> (t = tleft t1 \/ t = tright t1)}.
+  Proof.
+    exists (match t with
+       | tleft t' => Some t'
+       | tright t' => Some t'
+       | _ => None end).
+    destruct t; steps.
+Qed.      
+
+(* Idea: erase at top level *)
 Fixpoint ss_eval (t: tree) {struct t}: (option tree) :=
   match t with
     | ite ttrue t1 t2 => Some t1
@@ -40,56 +82,237 @@ Fixpoint ss_eval (t: tree) {struct t}: (option tree) :=
                    | true => option_map (pp e1) (ss_eval e2)
                    | false => option_map (fun e => pp e e2) (ss_eval e1) end
 
-    | pi1 (pp e1 _ ) => Some e1
-    | pi1 t => option_map pi1 (ss_eval t)
-    | pi2 (pp _ e2) => Some e2
-    | pi2 t => option_map pi2 (ss_eval t)
+    | pi1 t => match getPair t with
+              | exist _ None _  => option_map pi1 (ss_eval t)
+              | exist _ (Some (e1, e2)) _ => if (isValue e1) && (isValue e2) then Some e1 else option_map pi1 (ss_eval t) end
 
-    | lambda x t_body => Some (notype_lambda t_body) (* remove type annotation *)
-    | app (notype_lambda t_body) t2 => match (isValue t2) with
-                                  | true => Some (open 0 t_body t2)
-                                  | false => option_map (app (notype_lambda t_body)) (ss_eval t2) end
-    | app t1 t2 => match (isValue t2) with
-                    | false => option_map (app t1) (ss_eval t2)
-                    | true => option_map (fun e => app e t2) (ss_eval t1) end
-
-    | notype_tfix t_body => Some (open 0 t_body (notype_tfix t_body))
-
-    | notype_tlet t1 t_body => match isValue t1 with
-                               | true => Some (open 0 t_body t1)
-                               | false => option_map (fun e => notype_tlet e t_body) (ss_eval t1) end
+    | pi2 t => match getPair t with
+              | exist _ None _ => option_map pi2 (ss_eval t)
+              | exist _ (Some (e1, e2)) _ => if (isValue e1) && (isValue e2) then Some e2 else option_map pi2 (ss_eval t) end
+                                                                                                       
+    | app t1 t2 => match (isValue t1) with
+                    | true => match getApp t1 with
+                             | exist _ None _ => option_map (app t1) (ss_eval t2) 
+                             | exist _ (Some ts) _ => if (isValue t2) then Some (open 0 ts t2)
+                                                     else option_map (app t1) (ss_eval t2) end
+                    | false => option_map (fun e => app e t2) (ss_eval t1) end
+                    
+    | notype_tfix ts => Some (open 0 (open 1 ts zero) (notype_tfix ts))
 
     | succ t => option_map succ (ss_eval t)
-    | tmatch zero t0 _ => Some t0
-    | tmatch (succ ts) t0 t1 => match (isNat ts) with
-                                | true => Some (open 0 t1 ts)
-                                | false => option_map (fun e => tmatch (succ e) t0 t1) (ss_eval ts) end
-    | tmatch ts t0 t1 => match (isValue ts) with
-                          | true => None
-                          | false => option_map (fun e => tmatch e t0 t1) (ss_eval ts) end
+                          
+    | tmatch t1 t0 ts => match t1 with
+                       | zero => Some t0
+                       | succ t2 => if (isValue t2) then Some (open 0 ts t2) else option_map (fun e => tmatch e t0 ts) (ss_eval t1)
+                       | _ => option_map (fun e => tmatch e t0 ts) (ss_eval t1) end
 
+    | sum_match t tl tr => if (isValue t) then match t with
+                                   | tleft v => Some (open 0 tl v)
+                                   | tright v => Some (open 0 tr v)
+                                   | _ => None end
+                          else option_map (fun e => sum_match e tl tr) (ss_eval t)
+                            
+                          
+                                           (*
     | sum_match (tleft v) tl tr => match (isValue v) with
                                     | true => Some (open 0 tl v)
                                     | false => option_map (fun e => sum_match (tleft e) tl tr) (ss_eval v) end
     | sum_match (tright v) tl tr => match (isValue v) with
                                     | true => Some (open 0 tr v)
                                     | false => option_map (fun e => sum_match (tright e) tl tr) (ss_eval v) end
-
+*)
     | tleft t => option_map tleft (ss_eval t)
     | tright t => option_map tright (ss_eval t)
 
-    | tsize t => Some (build_nat (tsize_semantics t))
+    | tsize t => if (isValue t) then Some (build_nat (tsize_semantics t)) else (option_map tsize (ss_eval t))
+    | boolean_recognizer r t => if (isValue t) then match r with
+                                                   | 0 => Some (is_pair t)
+                                                   | 1 => Some (is_succ t)
+                                                   | 2 => Some (is_lambda t)
+                                                   | _ => None end
+                               else option_map (boolean_recognizer r) (ss_eval t)
     | _ => None
   end.
+
+
+    
+
+
+Ltac options :=
+unfold option_map in *.
+
+Ltac invert_constructor_equalities :=
+match goal with
+| H: ?F _ = ?F _ |- _ => is_constructor F; inversion H; clear H
+| H: ?F _ _ = ?F _ _ |- _ => is_constructor F; inversion H; clear H
+| H: ?F _ _ _ = ?F _ _ _ |- _ => is_constructor F; inversion H; clear H
+| H: ?F _ _ _ _ = ?F _ _ _ _ |- _ => is_constructor F; inversion H; clear H
+| H: ?F _ _ _ _ _ = ?F _ _ _ _ _ |- _ => is_constructor F; inversion H; clear H
+| H: ?F _ _ _ _ _ _ = ?F _ _ _ _ _ _ |- _ => is_constructor F; inversion H; clear H
+end. 
+
+Ltac custom_light :=
+  (intros) ||
+  (subst).
+
+Ltac destruct_match :=
+match goal with
+| [ |- context[match ?t with _ => _ end]] =>
+let matched := fresh "matched" in
+destruct t eqn:matched
+| [ H: context[match ?t with _ => _ end] |- _ ] =>
+let matched := fresh "matched" in
+destruct t eqn:matched
+end. 
+
+Lemma matchApp : forall T t b1 (b2: T) ,
+   ( exists t_body, t = notype_lambda t_body /\ 
+    match t with
+      | notype_lambda t_body => b1 t_body
+      | _ => b2 end = b1 t_body ) \/
+   ( match t with
+      | notype_lambda t_body => b1 t_body
+      | _ => b2 end = b2 ).
+  Proof.
+  destruct t; repeat steps; eauto.
+Qed.
+
+Ltac destruct_exists :=
+match goal with
+| H: exists x, _ |- _ => let freshX := fresh x in
+                  let matched := fresh "matched_exists" in
+                  destruct H as [ freshX ] eqn:matched
+end.
+
+Ltac instantiate_eq_refl :=
+match goal with
+| H: _ |- _ => pose proof (proj1 (H _) eq_refl); clear H
+| H: _ |- _ => pose proof (proj2 (H _) eq_refl); clear H
+| H: _ |- _ => pose proof (H _ eq_refl); clear H
+| H: _ |- _ => pose proof (H _ _ eq_refl ); clear H
+| H: _ |- _ => pose proof (proj1 (H _ _ ) eq_refl); clear H
+| H: _ |- _ => pose proof (proj2 (H _ _ ) eq_refl); clear H
+| H: _ |- _ => pose proof (H _ _ _ eq_refl); clear H
+| H: _ |- _ => pose proof (H _ _ _ _ eq_refl); clear H
+| H: _ |- _ => pose proof (H _ _ _ _ _ eq_refl); clear H
+| H: _ |- _ => pose proof (H _ _ _ _ _ _ eq_refl); clear H
+end.
+                                               
+Ltac destruct_sig :=
+match goal with
+  | H: _ |- context[let _ := getPair ?t in _ ]  => let fresh := fresh "getPair" in destruct (getPair t) (* eqn:fresh *)
+  | H: context[getPair ?t = _ ] |- _ => let fresh := fresh "getPair" in destruct (getPair t) (* eqn:fresh *)
+  | H: context[_ = getPair ?t ] |- _ => let fresh := fresh "getPair" in destruct (getPair t) (* eqn:fresh *)
+  | H: context[getApp ?t = _ ] |- _ => let fresh := fresh "getApp" in destruct (getApp t) (* eqn:fresh *)
+  | H: context[_ = getApp ?t ] |- _ => let fresh := fresh "getApp" in destruct (getApp t) (* eqn:fresh *)
+end. (* match on type of t = sig *)
+
+
+
+
+
+
+Lemma ss_eval_step : forall t t', ss_eval t = Some t' -> isValue t = true -> False.
+Proof.
+  induction t; repeat step || options.
+Qed.
+
+
+
+Lemma ss_eval_correct2: forall t t',(pfv t term_var = nil) -> scbv_step t t' ->  ss_eval t = Some t'.
+  intros.
+  induction H0 ; repeat light || list_utils || bools || options || invert_constructor_equalities || instantiate_eq_refl || rewrite <- isValueCorrect in *.
+  - (* pi1 *)
+     repeat light || options || bools || instantiate_eq_refl || invert_constructor_equalities || rewrite <- isValueCorrect in * || destruct_sig || destruct_match ; eauto with smallstep values.
+  - (* pi2 *)
+    repeat light || options || bools || instantiate_eq_refl || invert_constructor_equalities || rewrite <- isValueCorrect in * || destruct_sig || destruct_match ; eauto with smallstep values.
+  - (* notype_lambda *)
+    repeat light || options || bools || instantiate_eq_refl || invert_constructor_equalities || rewrite <- isValueCorrect in * || destruct_sig || destruct_match ; eauto with smallstep values.
+  - (* ite *)
+     repeat light || options || bools || instantiate_eq_refl || invert_constructor_equalities || rewrite <- isValueCorrect in * || destruct_sig || destruct_match ; eauto with smallstep values.    
+  - (* ite2 *)
+     repeat light || options || bools || instantiate_eq_refl || invert_constructor_equalities || rewrite <- isValueCorrect in * || destruct_sig || destruct_match ; eauto with smallstep values.   
+  - (* tfix *)
+     repeat light || options || bools || instantiate_eq_refl || invert_constructor_equalities || rewrite <- isValueCorrect in * || destruct_sig || destruct_match ; eauto with smallstep values.     
+  - (* tmatch *)
+    repeat light || options || bools || instantiate_eq_refl || invert_constructor_equalities || rewrite <- isValueCorrect in * || destruct_sig || destruct_match ; eauto with smallstep values.
+  - (* notype_lambda *)
+    repeat light || options || bools || instantiate_eq_refl || invert_constructor_equalities || rewrite <- isValueCorrect in * || destruct_sig || destruct_match ; eauto with smallstep values.
+  - repeat light || options || bools || instantiate_eq_refl || invert_constructor_equalities || rewrite <- isValueCorrect in * || destruct_sig || destruct_match ; eauto with smallstep values.
+  - repeat light || options || bools || instantiate_eq_refl || invert_constructor_equalities || rewrite <- isValueCorrect in * || destruct_sig || destruct_match ; eauto with smallstep values.
+    -     pose proof (ss_eval_step _ _ H). destruct (isValue t1). congruence. rewrite H. reflexivity.
+  - (* sum_match2 *)
+    pose proof (ss_eval_step _ _ H). destruct (isValue t1). congruence. rewrite H. rewrite H0. destruct (getApp v) eqn:A . destruct x. reflexivity. reflexivity.
+  - repeat light || options || bools || instantiate_eq_refl || invert_constructor_equalities || rewrite <- isValueCorrect in * || destruct_sig || destruct_match ; eauto using ss_eval_step with smallstep values.
+    pose proof (ss_eval_step _ _ H matched). inversion H3.
+    pose proof (ss_eval_step _ _ H matched). inversion H3.
+- repeat light || options || bools || instantiate_eq_refl || invert_constructor_equalities || rewrite <- isValueCorrect in * || destruct_sig || destruct_match ; eauto with smallstep values.    
+- repeat light || options || bools || list_utils || instantiate_eq_refl || invert_constructor_equalities || rewrite <- isValueCorrect in * || destruct_sig || destruct_match ; eauto with smallstep values.    
+    pose proof (ss_eval_step _ _  matched0 H4). inversion H1.
+-  repeat light || options || bools || instantiate_eq_refl || invert_constructor_equalities || rewrite <- isValueCorrect in * || destruct_sig || destruct_match ; eauto with smallstep values.    
+    pose proof (ss_eval_step _ _  matched0 H3). inversion H1.
+- repeat light || options || bools || instantiate_eq_refl || invert_constructor_equalities || rewrite <- isValueCorrect in * || destruct_sig || destruct_match ; eauto with smallstep values.    
+
+- repeat light || options || bools || instantiate_eq_refl || invert_constructor_equalities || rewrite <- isValueCorrect in * || destruct_sig || destruct_match ; eauto with smallstep values.
+      pose proof (ss_eval_step _ _  matched0 matched). inversion H.
+
+
+- repeat light || options || bools || instantiate_eq_refl || invert_constructor_equalities || rewrite <- isValueCorrect in * || destruct_sig || destruct_match ; eauto with smallstep values.    
+
+- repeat light || options || bools || instantiate_eq_refl || invert_constructor_equalities || rewrite <- isValueCorrect in * || destruct_sig || destruct_match ; eauto with smallstep values.    
+
+- repeat light || options || bools || instantiate_eq_refl || invert_constructor_equalities || rewrite <- isValueCorrect in * || destruct_sig || destruct_match ; eauto with smallstep values.    
+
+- destruct (isValue t1) eqn:V .  pose proof (ss_eval_step _ _   H V). inversion H2.
+  destruct t1 eqn: T ; repeat light || options || list_utils || bools || instantiate_eq_refl || invert_constructor_equalities || rewrite <- isValueCorrect in * || destruct_sig || destruct_match ; eauto with smallstep values.
+      
+- pose proof ss_eval_step. repeat light || options || bools || instantiate_eq_refl || invert_constructor_equalities || rewrite <- isValueCorrect in * || destruct_sig || destruct_match ; eauto using ss_eval_step with smallstep values.    pose proof (H2 t1 t2 H1 matched). inversion H3.
+  
+
+- rewrite H1. destruct (isValue t1) eqn:V .  pose proof (ss_eval_step _ _   H1 V). inversion H2. reflexivity.
+
+
+Qed.
+
+     
+Lemma ss_eval_correct1: forall t t', ss_eval t = Some t' -> (pfv t term_var = nil) -> scbv_step t t'.
+Proof.
+  induction t; intros ; repeat light.
+  - (* size *)
+    repeat light || options || destruct_match || invert_constructor_equalities || rewrite isValueCorrect in * ; eauto using fv_nil_top_level_var with smallstep.
+  - repeat light || invert_constructor_equalities || instantiate_eq_refl || list_utils || destruct_match || options || rewrite isValueCorrect in *; eauto with smallstep values.
+    rewrite (proj1 (i t) eq_refl) ; eauto with smallstep values.
+  - repeat light || invert_constructor_equalities || instantiate_eq_refl || options || bools || list_utils || rewrite isValueCorrect in * || destruct_sig || destruct_match ; eauto with smallstep values.
+  - repeat light || invert_constructor_equalities || instantiate_eq_refl || options || bools || list_utils || rewrite isValueCorrect in * || destruct_sig || destruct_match ; eauto with smallstep values.
+  - repeat light || invert_constructor_equalities || instantiate_eq_refl || options || bools || list_utils || rewrite isValueCorrect in * || destruct_sig || destruct_match ; eauto with smallstep values.
+  - destruct (ss_eval t1) ; repeat light || invert_constructor_equalities || destruct_match || list_utils || options || rewrite isValueCorrect in *; eauto with smallstep values.
+  - destruct (isValue t) eqn:V;  repeat light || invert_constructor_equalities || list_utils || destruct_match || options || rewrite isValueCorrect in * || eauto using isValueCorrect, fv_nil_top_level_var with smallstep values || unfold option_map in H.
+  - (* tleft *) repeat light || invert_constructor_equalities || list_utils || destruct_match || options || rewrite isValueCorrect in *; eauto with smallstep values.
+
+  - (* Tmatch *) destruct (ss_eval t1); repeat light || invert_constructor_equalities || list_utils || options || destruct_match || rewrite isValueCorrect in *; eauto with smallstep values.
+  - (* notype_tfix *) repeat light || invert_constructor_equalities || list_utils || destruct_match || options || rewrite isValueCorrect in *; eauto with smallstep values.
+  - (* tright *) repeat light || invert_constructor_equalities || list_utils || destruct_match || options || rewrite isValueCorrect in *; eauto with smallstep values.
+  - (* tleft *) repeat light || invert_constructor_equalities || list_utils || destruct_match || options || rewrite isValueCorrect in *; eauto with smallstep values.
+  - (* sum_match *) repeat light || invert_constructor_equalities || list_utils || destruct_match || options || rewrite isValueCorrect in *; eauto with smallstep values.
+Qed.  
+
+
+
+    
 
 Fixpoint ss_eval_n (t : tree) (k: nat) : (option tree) :=
   match k with
     | 0 => Some t
     | S k' => match ss_eval t with
                | Some t' => ss_eval_n t' k'
-               | None => None end
+               | None => Some t end
                end.
+Require Extraction.
+Extraction Language Ocaml.
+Set Extraction AccessOpaque.
 
+
+Extraction "evaluator.ml" ss_eval_n.
 
 Definition example1 :=
 [|
@@ -108,9 +331,18 @@ Definition example1 :=
              end)
         end))
     in
-    (fibo (s (s (s (s 1)))))
+    (fibo (s (s (s (s (s (s (s (s (s (s (s (s 1)))))))))))))
  |].
 
+Fixpoint treeToNat (t: tree) :=
+  match t with
+    | zero => 0
+    | succ t' => S (treeToNat t')
+    |_ => 0 end.
+
+Eval compute in option_map treeToNat (ss_eval_n example1 50000).
+Example fibo377 : option_map treeToNat (ss_eval_n example1 50000) = Some 377.
+Proof. reflexivity. Qed.
 Example fibo4 : (ss_eval_n example1 141) =  Some (succ (succ (succ (succ (succ (succ (succ (succ zero)))))))).
 Proof. compute. reflexivity. Qed.
 
@@ -134,7 +366,10 @@ Definition example2 :=
     (fibo (s (s (s (s (s 1))))))
  |].
 
+Check example2.
+
+Eval compute in ss_eval_n example2 254.
 
 Eval compute in ss_eval_n example2 254.
 Example fibo5 : (ss_eval_n example2 254) =  Some (succ (succ (succ (succ (succ (succ (succ (succ (succ (succ (succ (succ (succ zero))))))))))))).
-Proof. compute. reflexivity. Qed.
+Proof. reflexivity. Qed.
